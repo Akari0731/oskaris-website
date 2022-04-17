32b101b24591064dacbbdabfc9310336216d92a4
Akari Nishii <nishii0731@gmail.com>
2022-04-17 10:23:05 +0200

    Restructuring and add video page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { MusicButtonComponent } from './components/music-button/music-button.component';
 import { SectionComponent } from './components/section/section.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VideoComponent } from './pages/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     HeroComponent,
     HeaderComponent,
     MusicButtonComponent,
     SectionComponent,
     FooterComponent,
+    VideoComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/pages/video/video.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.sass'],
+})
+export class VideoComponent {
+  constructor() {
+    console.log('VideoComponent');
+  }
+}
