334a92d078fb746b935a8a8612199fbdd8e1c161
Akari Nishii <nishii0731@gmail.com>
2022-04-23 13:24:52 +0200

    Test

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,11 +6,21 @@ import { Router } from '@angular/router';
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.sass'],
 })
-export class VideoComponent implements OnInit {
+export class VideoComponent implements OnInit, AfterViewInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
+    document.domain = 'player.vimeo.com';
     window.scrollTo(0, 0);
+  }
+
+  ngAfterViewInit() {
+    const iframe = document.getElementById('iframe') as any;
+    const iframDoc = iframe.contentDocument;
+    if (iframDoc?.body?.style) {
+      iframDoc.body.style.color = 'blue !important';
+    }
+    console.log('iframe', iframe, iframDoc, iframDoc?.body?.style);
   }
 
   onNavigateHome() {
